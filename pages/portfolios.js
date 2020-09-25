528415b907724ab98025885cf7e3877d417b76c7
Preil <preil2000@gmail.com>
2020-09-25 18:42:48 +0300

    Getting dummy data for portfolio from jsonplaceholder using axios.

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import axios from 'axios'
+
 
 class Portfolios extends React.Component {
+
+  static async getInitialProps() {
+    let posts = []
+    try {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      posts = res.data
+    } catch (e) {
+      console.error(e)
+    }
+
+    return {posts: posts.slice(0, 10)}
+  }
+
   render() {
+    const {posts} = this.props
     return (
-      <h1>I am Portfolio</h1>
+      <>
+        <h1>I am Portfolio</h1>
+        <ul>
+          {posts.map(post => <li key={post.id}>{post.id}</li>)}
+        </ul>
+      </>
+
+
     )
   }
 }
