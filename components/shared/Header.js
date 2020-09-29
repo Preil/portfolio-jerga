7cc0ec176519cc9e06433eb25f2b9b957df203b8
Preil <preil2000@gmail.com>
2020-09-29 08:58:58 +0300

    Header adjustment part1.

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,29 +1,93 @@
-import React from 'react'
+import React, {useState} from 'react';
 import Link from 'next/link'
-import {Button} from 'reactstrap'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <Button outline color="primary">
-          <Link href="/">Home</Link>
-        </Button>{' '}
-        <Link href="/about">
-          <a>About </a>
-        </Link>
-        <Link href="/portfolios">
-          <a>Portfolios </a>
-        </Link>
-        <Link href="/blogs">
-          <a>Blogs </a>
-        </Link>
-        <Link href="/cv">
-          <a>CV </a>
-        </Link>
-      </div>
-    )
-  }
+const BsNavLink = (props) => {
+  const {route, title} = props;
+  return (
+    <Link href={route}>
+      <a className="nav-link">{title}</a>
+    </Link>
+  )
+
+
 }
 
-export default Header
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Ilya Preil</NavbarBrand>
+        <NavbarToggler onClick={toggle}/>
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+
+            <NavItem>
+              <BsNavLink route="/" title="Home"/>
+            </NavItem>
+            <NavItem>
+              <BsNavLink route="/about" title="About"/>
+            </NavItem>
+            <NavItem>
+              <BsNavLink route="/portfolios" title="Portfolios"/>
+            </NavItem>
+            <NavItem>
+              <BsNavLink route="/blogs" title="Blogs"/>
+            </NavItem>
+            <NavItem>
+              <BsNavLink route="/cv" title="CV"/>
+            </NavItem>
+
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
+
+
+// import React from 'react'
+// import Link from 'next/link'
+// import {Button} from 'reactstrap'
+//
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Button outline color="primary">
+//           <Link href="/">Home</Link>
+//         </Button>{' '}
+//         <Link href="/about">
+//           <a>About </a>
+//         </Link>
+//         <Link href="/portfolios">
+//           <a>Portfolios </a>
+//         </Link>
+//         <Link href="/blogs">
+//           <a>Blogs </a>
+//         </Link>
+//         <Link href="/cv">
+//           <a>CV </a>
+//         </Link>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default Header
