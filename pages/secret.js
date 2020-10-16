439c75cf8ab59a03537a4b4bba50b3b1f1c696fc
Preil <preil2000@gmail.com>
2020-10-16 13:24:42 +0300

    Secure server end point part1. Displaying information on secrete page from the server.

--- pages/secret.js
+++ pages/secret.js
@@ -1,24 +1,59 @@
 import React from 'react';
 import BasePage from '../components/BasePage';
 import withAuth from '../components/hoc/withAuth';
+import * as axios from "axios";
 
 
 class Secret extends React.Component {
 
-  static getInitialProps(){
+  static getInitialProps() {
     const superSecretValue = 'Super secret value'
     return {superSecretValue}
+  }
+
+  constructor(props) {
+    super();
+    this.state = {
+      secretData: []
+    }
+  }
+
+  async componentDidMount() {
+    const res = await axios.get('/api/v1/secret');
+    const secretData = res.data;
+
+    this.setState({
+      secretData
+    });
+  }
+
+  displaySecretData() {
+    const {secretData} = this.state;
+    if (secretData && secretData.length > 0) {
+      return (
+        secretData.map((data, index) => {
+          return (
+            <div key={index}>
+              <p>{data.title} </p>
+              <p> {data.description} </p>
+            </div>
+          )
+        })
+      )
+    }
   }
 
   render() {
 
     const {superSecretValue} = this.props;
+    console.log(this.state)
 
     return (
       <BasePage className="about-page">
         <h1>I am Secret page</h1>
         <p>Secret content is here</p>
         <p>{superSecretValue}</p>
+        {this.displaySecretData()}
       </BasePage>
     )
   }
--- server/index.js
+++ server/index.js
@@ -7,8 +7,23 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = routes.getRequestHandler(app);
 
+const secretData = [
+  {
+    title: 'SecretData 1',
+    description: 'Plans how to build spaceship'
+  },
+  {
+    title: 'SecretData 2',
+    description: 'Me secrets'
+  }
+];
+
 app.prepare().then(() => {
   const server = express();
+
+  server.get('/api/v1/secret', (req, res) => {
+    return res.json(secretData);
+  });
 
   server.all('*', (req, res) => {
     return handle(req, res);
