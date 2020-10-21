90afee39148af8a7a2f483239109f79087a32ac6
Preil <preil2000@gmail.com>
2020-10-21 15:09:20 +0300

    Portfolios styles added.

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import axios from 'axios'
+import {
+  Col, Row, Card, CardHeader, CardText, CardBody,
+  CardTitle,
+} from 'reactstrap';
 import BaseLayout from "../components/layouts/BaseLayout";
 
 import BasePage from '../components/BasePage'
@@ -21,27 +25,39 @@ class Portfolios extends React.Component {
     return {posts: posts.slice(0, 10)}
   }
 
+  renderPosts(posts) {
+    return posts.map((post, index) => {
+      return (
+        <Col md="4">
+          <React.Fragment key={index}>
+              <span>
+                <Card className="portfolio-card">
+                  <CardHeader className="portfolio-card-header">Some Position {index}</CardHeader>
+                  <CardBody>
+                    <p className="portfolio-card-city"> Some Location {index} </p>
+                    <CardTitle className="portfolio-card-title">Some Company {index}</CardTitle>
+                    <CardText className="portfolio-card-text">Some Description {index}</CardText>
+                    <div className="readMore"> </div>
+                  </CardBody>
+                </Card>
+              </span>
+          </React.Fragment>
+        </Col>
+      )
+    })
+  }
+
+
   render() {
     const {posts} = this.props
     return (
-      <BaseLayout className="cover" {...this.props.auth}>
-        <BasePage title="Portfolios">
-
-          <ul>
-            {posts.map(post =>
-              <li key={post.id}>
-                <Link
-                  route={`/portfolios/${post.id}`}>
-                  <a key={post.id}>
-                    {post.title}
-                  </a>
-                </Link>
-              </li>
-            )}
-          </ul>
+      <BaseLayout {...this.props.auth}>
+        <BasePage className="portfolio-page" title="Portfolios">
+          <Row>
+            {this.renderPosts(posts)}
+          </Row>
         </BasePage>
       </BaseLayout>
-
     )
   }
 }
