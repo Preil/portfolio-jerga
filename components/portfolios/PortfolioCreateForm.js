edfb0c702d4a2018e6d2e63bf14989a1a24ebd97
Preil <preil2000@gmail.com>
2020-10-26 12:59:27 +0200

    Fields validating part1. All field is required.

--- components/portfolios/PortfolioCreateForm.js
+++ components/portfolios/PortfolioCreateForm.js
@@ -3,15 +3,16 @@ import {Formik, Form, Field, ErrorMessage} from 'formik';
 import {Button, FormGroup, Label} from 'reactstrap';
 import PortImport from "../form/PortInput";
 
-const validateInputs = (validate) => {
+const validateInputs = (values) => {
   let errors = {};
-  // if (!values.email) {
-  //   errors.email = 'Required';
-  // } else if (
-  //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-  // ) {
-  //   errors.email = 'Invalid email address';
-  // }
+
+  Object.entries(values).forEach(([key, value]) => {
+    if(!values[key]) {
+      errors[key] = `Field ${key} is Required!`
+    }
+  })
+
+
   return errors;
 }
 
