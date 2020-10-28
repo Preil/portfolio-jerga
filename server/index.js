e9869c203afcc549618fccd984bf2bdc70443368
Preil <preil2000@gmail.com>
2020-10-28 19:10:05 +0200

    Server Book CRUD endpoints created.

--- server/index.js
+++ server/index.js
@@ -54,9 +54,49 @@ app.prepare().then(() => {
   });
 
 
+  server.get('/api/v1/books', (req, res) => {
+    Book.find({}, (err, allBooks) => {
+      if (err) {
+        return res.status(422).send(err);
+      }
+      return res.json(allBooks);
+    })
+  });
+
+  server.patch('/api/v1/books/:id', (req, res)=>{
+    const bookId = req.params.id;
+    const bookData = req.body;
+
+    Book.findById(bookId, (err, foundBook)=>{
+      if (err) {
+        return res.status(422).send(err);
+      }
+      foundBook.set(bookData);
+      foundBook.save((err, savedBook)=>{
+        if (err) {
+          return res.status(422).send(err);
+        }
+        return res.json(savedBook);
+      })
+    })
+  })
+
+  server.delete('/api/v1/books/:id', (req, res) => {
+    const bookId = req.params.id;
+
+    Book.deleteOne({_id: bookId}, (err, deletedBook) => {
+      if (err) {
+        return res.status(422).send(err);
+      }
+      return res.json({status: 'DELETED'});
+    })
+  });
+
+
   server.get('/api/v1/secret', authService.checkJWT, (req, res) => {
     return res.json(secretData);
   });
+
 
   server.get('/api/v1/onlysiteowner', authService.checkJWT, authService.checkRole('siteOwner'), (req, res) => {
     return res.json(secretData);
