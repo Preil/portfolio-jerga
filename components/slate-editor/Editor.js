9f00c072f2796282eeb5b6bf742891fc45b99a03
Preil <preil2000@gmail.com>
2020-11-09 17:36:27 +0200

    Custom blocks slate example.

--- components/slate-editor/Editor.js
+++ components/slate-editor/Editor.js
@@ -26,6 +26,15 @@ const initialValue = Value.fromJSON({
   }
 })
 
+// Renderer for code blocks
+const CodeNode = (props) =>{
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
 export default class SlateEditor extends React.Component {
   state = {
     value: initialValue,
@@ -43,25 +52,42 @@ export default class SlateEditor extends React.Component {
   }
 
   onKeyDown = (event, editor, next)=>{
-    if(event.key !== '&') return next()
+    if(event.key !== 'x' || !event.ctrlKey) return next()
 
     //Prevent the ampersand character from being inserted.
     event.preventDefault();
 
-    //Change the value by inserting 'and' at the cursor's position.
-    editor.insertText('and')
+    // Determine whether any of current selected blocks are code blocks
+    const isCode = editor.value.blocks.some(block => block.type == 'code')
+
+    // Toggle the block type depending on 'isCode'
+    editor.setBlocks(isCode ? 'paragraph' : 'code')
     return true;
   }
+
+  renderNode = (props, editor, next) =>{
+    switch(props.node.type) {
+      case 'code':
+        return <CodeNode {...props} />
+      case 'paragraph':
+        return <p{...props.attributes}>{props.children}</p>
+      default:
+        return next()
+    }
+  }
+
 
   render() {
     const {isLoaded} = this.state;
     return (
       <>
+        <pre><code>This is code block rendered</code></pre>
         {
           isLoaded &&
           <Editor value={this.state.value}
                   onChange={this.onChange}
                   onKeyDown={this.onKeyDown}
+                  renderNode={this.renderNode}
           />
         }
       </>
